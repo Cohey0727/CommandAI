c9cb7a5b1a9abab027abb577135fb309c55ecacb
KoheiOkamoto <ohayousagi.ac.kook0727@gmail.com>
2024-02-04 10:48:28 +0900

    main

---
+++ packages/frontend/src/App.module.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "App": string;
+  readonly "header": string;
+  readonly "link": string;
+  readonly "logo": string;
+  readonly "logo-spin": string;
+};
+export = styles;
+
